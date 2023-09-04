fbde287a468e6dfe22c9e4248780cfcf865d5bd4
Simon Emms <simon@simonemms.com>
2023-09-04 21:06:31 +0000

    chore: cruft update

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: process.env.VITE_HOST,
-		port: Number(process.env.VITE_PORT ?? 5173)
+		port: Number(process.env.VITE_PORT ?? 5173),
 	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
 });
